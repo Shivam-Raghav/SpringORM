8830f82164c7912eef5ce6f89f1723dca6111734
yatendra-agnisys <121540329+yatendra-agnisys@users.noreply.github.com>
2023-09-13 01:06:35 +0530

    issue fix

--- springorm/src/main/java/com/Spring/orm/dao/StudentDao.java
+++ springorm/src/main/java/com/Spring/orm/dao/StudentDao.java
@@ -6,50 +6,24 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.Spring.orm.entities.Student;
 
-import java.util.List;
-
-import javax.transaction.Transaction;
 public class StudentDao {
-	
+
 	private HibernateTemplate hibernateTemplate;
-	//save student
+
 	@Transactional
 	public int insert(Student student) {
-		//insert
-	Integer i=(Integer)this.hibernateTemplate.save(student);
+
+		Integer i = (Integer) this.hibernateTemplate.save(student);
 		return i;
 	}
-//	////get the single data(object)
-//	public Student  getStudent(int studentId) {
-//Stud//ent student=this.hibernateTemplate.get(Student.class,studentId);
-  //ret//urn student;		
-	//}//	
-	//get all students(all rows)
-   //public List<Student>getAllStudents(){
-//List<Student>students=this.hibernateTemplate.loadAll(Student.class);
-	//return students;   
-//   }
-	//deleting the data
-   //@Transactional
-    //public void deleteStudent(int studentId) {
-    //	Student student=this.hibernateTemplate.get(Student.class,studentId);
-    //	this.hibernateTemplate.delete(student);
-    	
-    //}
-    //updating data
-   //@Transactional
-   // public void updateStudent(Student student) {
-    //	this.hibernateTemplate.update(student);
-    	
-  //  }
-    
-	
-	//public HibernateTemplate getHibernateTemplate() {
-		//return hibernateTemplate;
-	//}
-//	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
-	//	this.hibernateTemplate = hibernateTemplate;
-//	}
+
+	public HibernateTemplate getHibernateTemplate() {
+		return hibernateTemplate;
+	}
+
+	public void setHibernateTemplate(HibernateTemplate hibernateTemplate) {
+		this.hibernateTemplate = hibernateTemplate;
+	}
 	
 	
 
--- springorm/src/main/java/com/Spring/orm/entities/Student.java
+++ springorm/src/main/java/com/Spring/orm/entities/Student.java
@@ -6,61 +6,45 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="student_details")
+@Table(name = "student_details")
 public class Student {
-@Id	
-@Column(name="student_id")
-private int studentId;
-@Column(name="student_name")
- private String studentName;
-@Column(name="student_city")
- private String studentCity;
+	@Id
+	@Column(name = "student_id")
+	private int studentId;
+	@Column(name = "student_name")
+	private String studentName;
+	@Column(name = "student_city")
+	private String studentCity;
 
+	public Student(int studentId, String studentName, String studentCity) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentCity = studentCity;
+	}
 
-public Student(int studentId, String studentName, String studentCity) {
-	super();
-	this.studentId = studentId;
-	this.studentName = studentName;
-	this.studentCity = studentCity;
-}
+	public int getStudentId() {
+		return studentId;
+	}
 
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
 
-public Student() {
-	
-}
+	public String getStudentName() {
+		return studentName;
+	}
 
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
 
-public int getStudentId() {
-	return studentId;
-}
+	public String getStudentCity() {
+		return studentCity;
+	}
 
+	public void setStudentCity(String studentCity) {
+		this.studentCity = studentCity;
+	}
 
-public void setStudentId(int studentId) {
-	this.studentId = studentId;
-}
-
-
-public String getStudentName() {
-	return studentName;
-}
-
-
-public void setStudentName(String studentName) {
-	this.studentName = studentName;
-}
-
-
-public String getStudentCity() {
-	return studentCity;
-}
-
-
-public void setStudentCity(String studentCity) {
-	this.studentCity = studentCity;
-}
-
-
-
-
- 
 }
